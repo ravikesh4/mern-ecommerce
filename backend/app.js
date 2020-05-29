8179d83d02b877a5158dcbbd3921461599bcc45f
Ravikesh Yadav <ravikesh@netzary.com>
2020-05-29 22:00:37 +0530

    category and product api got created

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ require('dotenv').config()
 // import routes
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
 
 // app
 const app = express()
@@ -33,5 +35,7 @@ app.use(cookieParser());
 // routes middleware 
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', productRoutes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
